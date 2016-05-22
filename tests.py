206dbdea38733fb13edb434f9acc8f43ec0d23bb
er3456qi <er3456qi@outlook.com>
2016-05-22 08:49:55 +0800

    improve question method test

--- tests.py
+++ tests.py
@@ -23,6 +23,24 @@ class QuestionMethodTests(PollsTestCase):
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is older than 1 day.
+        """
+        time = datetime.datetime.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently())
+
+    def test_was_published_recently_with_recently_question(self):
+        """
+        was_published_recently() should return True for questions whose
+        pub_date is within the last day.
+        """
+        time = datetime.datetime.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertTrue(recent_question.was_published_recently())
+
 
 if __name__ == '__main__':
     unittest.main()
